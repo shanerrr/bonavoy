d735857b558c193e5180447e56c50c21c7c99543
Neilzon <neilcviloria@gmail.com>
2020-09-03 21:54:40 -0600

    route drawing on map

--- frontend/my-app/src/Components/Map/Map.js
+++ frontend/my-app/src/Components/Map/Map.js
@@ -4,21 +4,23 @@ import mapboxgl from 'mapbox-gl';
 import './style.css';
 import Plan from '../Plan/Plan';
 
-import Turf from '@turf/turf';
-
+import * as turf from '@turf/turf';
 
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './../../.env.local') });
+require('dotenv').config({ 
+   path: path.resolve(__dirname, './../../.env.local') 
+ });
 
 const TOKEN = process.env.REACT_APP_MAPS_API_KEY;
 mapboxgl.accessToken = TOKEN;
 
+var nothing = turf.featureCollection([]);
 class Map extends React.Component{
   
   constructor(props) {
     super(props);
     this.state = { // TODO: change lng and lat to be current position
-      lng: -113.41777,
+      lng: -113.41777, 
       lat: 53.48538,
       zoom: 10,
       markers:[],
@@ -28,6 +30,7 @@ class Map extends React.Component{
     this.addMarkerHandler = this.addMarkerHandler.bind(this);
     this.reorderMarkersHandler = this.reorderMarkersHandler.bind(this);
     this.removeMarkerHandler = this.removeMarkerHandler.bind(this);
+    this.updateRoute = this.updateRoute.bind(this);
     this.updateBBox = this.updateBBox.bind(this);
   }
 
@@ -35,22 +38,24 @@ class Map extends React.Component{
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.state.lng, this.state.lat], // TODO: get location of person
+      center: [this.state.lng, this.state.lat], // TODO: implement ask and get location of person
       zoom: this.state.zoom,
     });
-    this.map = map;
+
     // disable map rotation using right click + drag
     map.dragRotate.disable();
+
+    this.map = map;
   }
 
-  updateBBox(){
+  updateBBox() {
     let coordsList = this.state.markers.map((marker) => {
       return [marker._lngLat.lng, marker._lngLat.lat];
     });
 
-    let bbox = [[0,0], [0,0]]; // bbox for no points
+    let bbox = [[0,0], [0,0]]; // default bbox 
 
-    if(coordsList.length == 1){
+    if(coordsList.length === 1){
       bbox = [coordsList[0], coordsList[0]]; 
     } 
     else if(coordsList.length > 1) {
@@ -59,7 +64,7 @@ class Map extends React.Component{
       for(let coords of coordsList){
         // bottom left
         if(coords[0] < bbox[0][0]){
-          console.log(coords[0], bbox[0][0]);
+
           bbox[0][0] = coords[0];
         }
         if(coords[1] < bbox[0][1]){
@@ -80,6 +85,9 @@ class Map extends React.Component{
   }
 
   addMarkerHandler(coords){
+    // new stop
+
+    // update stops
     const marker = new mapboxgl.Marker()
       .setLngLat(coords)
       .addTo(this.map);
@@ -100,10 +108,8 @@ class Map extends React.Component{
     // fly to fit bounds
     this.map.fitBounds(bbox,{padding:padding, maxZoom:8});
 
-    // make request
-    // fetch(`https://api.mapbox.com/optimized-trips/v1/mapbox/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?access_token=pk.eyJ1IjoibmVpbHpvbiIsImEiOiJja2R5MjNkc3cyNDd5MnVudWVvaXptY3IyIn0.t7H18YFnJnci9cvjd3Q-Tg`)
-    //   .then((response) => response.json()).then((data) => console.log(data));
-
+    // make get new route
+    this.updateRoute()
 
   }
 
@@ -112,16 +118,83 @@ class Map extends React.Component{
     let result = this.state.markers;
     const marker = result.splice(index, 1)[0];
     this.setState({...this.state, markers:result});
+
     // remove from map
     marker.remove();
 
     // get updated route
-    
+    this.updateRoute();
   }
 
-  requestRoute(){
+  updateRoute(){
     // generate geoJSON data array from markers
+    const coords = this.state.markers.map((item) => {
+      const lng = item._lngLat.lng.toString();
+      const lat = item._lngLat.lat.toString();
+      return lng.concat(",",lat);
+    });
+    const coordString = coords.join(';');
+
     // make request 
+    if(this.state.markers.length >= 1 && this.state.markers.length <= 25){
+      fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${coordString}?annotations=distance,speed&geometries=geojson&access_token=${TOKEN}`)
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          if(response.code === 'NoRoute'){
+            console.log('no route');
+          }
+          else if(response.code === 'InvalidInput'){
+            console.log('invalid route')
+          } 
+          else if(response.code === 'Ok') {
+            const data = response.routes[0];
+            const duration = data.duration;
+            const distance = data.distance;
+            const route = data.geometry.coordinates;
+
+            const geojson = {
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'LineString',
+                coordinates: route
+              }
+            };
+            
+            // add source and layer if not exists
+            if (!this.map.getSource('route')) {
+              this.map.addLayer({
+                id: 'route',
+                type: 'line',
+                source: {
+                  type: 'geojson',
+                  data: {
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                      type: 'LineString',
+                      coordinates: geojson
+                    }
+                  }
+                },
+                layout: {
+                  'line-join': 'round',
+                  'line-cap': 'round'
+                },
+                paint: {
+                  'line-color': '#3887be',
+                  'line-width': 5,
+                  'line-opacity': 0.75
+                }
+              });
+            }
+
+            // add layer to map
+            this.map.getSource('route').setData(geojson);
+          }
+        });
+    }
   }
 
   reorderMarkersHandler(from, to){
@@ -131,6 +204,7 @@ class Map extends React.Component{
     this.setState({...this.state, markers:result});
 
     // update route
+    this.updateRoute();
   }
 
   render() {
