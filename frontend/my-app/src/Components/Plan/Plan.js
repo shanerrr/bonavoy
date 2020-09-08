982e8c58ac152ce8b509bbd42716f46d4639da6f
Neilzon <neilcviloria@gmail.com>
2020-09-07 18:35:53 -0600

    minor fixes

--- frontend/my-app/src/Components/Plan/Plan.js
+++ frontend/my-app/src/Components/Plan/Plan.js
@@ -13,37 +13,9 @@ class Plan extends React.Component {
 		this.state = {
 			showPlan:true,
 		}
-		// this.addStopHandler = this.addStopHandler.bind(this);
-		// this.reorderStopsHandler = this.reorderStopsHandler.bind(this);
-		// this.removeStopHandler = this.removeStopHandler.bind(this);
+
 		this.togglePlan = this.togglePlan.bind(this);
 	}
-	
-	// addStopHandler(stop){
-	// 	//add stop to state
-	// 	this.setState({stops:[...this.state.stops, stop]});
-	// 	//add new marker 
-	// 	this.props.addMarker(stop.center);
-	// }
-	
-	// reorderStopsHandler(from, to){
-	// 	let result = [...this.state.stops];
-	// 	const [removed] = result.splice(from, 1);
-	// 	result.splice(to, 0, removed);
-	// 	this.setState({...this.state,stops:result});		
-
-	// 	// reorder markers
-	// 	this.props.reorderMarkers(from, to);
-	// }
-
-	// removeStopHandler(index){
-	// 	let result = [...this.state.stops];
-	// 	result.splice(index, 1);
-	// 	this.setState({...this.state, stops:result});
-
-	// 	// remove marker
-	// 	this.props.removeMarker(index);
-	// }
 
 	togglePlan(){
 		this.setState({showPlan:!this.state.showPlan});
