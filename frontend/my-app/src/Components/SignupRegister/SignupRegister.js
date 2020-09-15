d83f6ef109d30f98de378ba99d0bcc4cd49d8185
Shane Goonasekera <46552619+shanerrr@users.noreply.github.com>
2020-09-14 21:31:05 -0600

    finsihed user-modal frontend

--- frontend/my-app/src/Components/SignupRegister/SignupRegister.js
+++ frontend/my-app/src/Components/SignupRegister/SignupRegister.js
@@ -2,6 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './SignupRegister.css';
 
 function SignupRegister(props) {
+
+  const inputs = document.querySelectorAll(".user-modal-input");
+  function addcl() {
+    let parent = this.parentNode.parentNode;
+    console.log(parent);
+    parent.classList.add("focus");
+  }
+
+  function remcl(){
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+      parent.classList.remove("focus");
+    }
+  }
+
+
+  inputs.forEach(input => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+  });
   return (
     <>
       <i className="user-modal-splash-image fas fa-times" onClick={props.handleClose}></i>
@@ -15,26 +35,39 @@ function SignupRegister(props) {
             <form className="user-modal-form"action="index.html">
               {/* <img src="images/login.png"/> */}
               <h2 className="user-modal-title">{props.ifLogin ? "Login" : "Sign Up"}</h2>
+
+                <div className={!props.ifLogin ? "user-modal-inputdiv email" : "display-none"}>
+                  <div className="user-modal-i">
+                    <i className="fas fa-envelope"></i>
+                  </div>
+                  <div>
+                    <h5>Email</h5>
+                    <input type="email" className="user-modal-input"/>
+                  </div>
+                </div>
+
                 <div className="user-modal-inputdiv username">
                   <div className="user-modal-i">
                     <i className="fas fa-user"></i>
                   </div>
                   <div>
                     <h5>Username</h5>
-                    <input type="text"/>
+                    <input type="text" className="user-modal-input"/>
                   </div>
                 </div>
+
                 <div className="user-modal-inputdiv pass">
                   <div className="user-modal-i"> 
                     <i className="fas fa-lock"></i>
                   </div>
-                <div>
-                  <h5>Password</h5>
-                  <input type="password"/>
+                  <div>
+                    <h5>Password</h5>
+                    <input type="password" className="user-modal-input"/>
+                  </div>
                 </div>
-                </div>
+
                   {props.ifLogin && <a className="user-modal-a" href="#">Forgot Password?</a>}
-                  <input type="submit" className="user-modal-btn" value="Login"/>
+                  <input type="submit" className="user-modal-btn" value={props.ifLogin ? "Login" : "Sign Up"}/>
               </form>
               
           </div>
@@ -43,7 +76,6 @@ function SignupRegister(props) {
           <a onClick={props.ifLogin ? "":""}>{props.ifLogin ? "Don't have an account? Sign up" : "Already have an account? Log in"}</a>
         </div>
       </div>
-      <script type="text/javascript" src="js/inputanimator.js"></script>
     </>    
   );
 }
