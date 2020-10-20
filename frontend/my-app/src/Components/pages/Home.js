5104cd454516ac4f21253991ab8cc9a669cd30d4
Shane Goonasekera <46552619+shanerrr@users.noreply.github.com>
2020-10-19 22:20:33 -0600

    dik

--- frontend/my-app/src/Components/pages/Home.js
+++ frontend/my-app/src/Components/pages/Home.js
@@ -3,7 +3,7 @@ import '../../App.css';
 import Cards from '../Cards/Cards';
 import FrontPage from '../FrontPage/FrontPage';
 import SecondPage from '../SecondPage/SecondPage'
-// import Footer from '../Footer';
+import Footer from '../Footer/Footer';
 
 function Home(props) {
 
@@ -11,7 +11,7 @@ function Home(props) {
     <>
       <FrontPage data={props.data}/>
       <SecondPage data={props.data}/>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
